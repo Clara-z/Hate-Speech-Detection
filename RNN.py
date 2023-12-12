ef8f6bda9cae0bd4a460e63b126c727a68405ffe
Qiang Zeng <qiangzen@usc.edu>
2023-12-11 23:55:17 -0800

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -1,32 +1,57 @@
-import tensorflow as tf
+import numpy as np
+import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.metrics import classification_report, confusion_matrix
 
-# Load your dataset
-# Assuming dataset is loaded into 'texts' and 'labels'
 
-# Step 1: Data Preprocessing
-tokenizer = Tokenizer(num_words=5000)  # Only considering the top 5000 words
-tokenizer.fit_on_texts(texts)
+def preprocess_data(data):
+    tokenizer = Tokenizer(num_words=5000, oov_token="<OOV>")
+    tokenizer.fit_on_texts(data[:, 1])
+    sequences = tokenizer.texts_to_sequences(data[:, 1])
+    padded_sequences = pad_sequences(sequences, maxlen=200)
+    return padded_sequences, data[:, 2].astype(int)
 
-sequences = tokenizer.texts_to_sequences(texts)
-padded_sequences = pad_sequences(sequences, maxlen=200)  # Assuming max length of sequences is 200
 
-# Step 2: Building the RNN Model
-model = Sequential()
-model.add(Embedding(input_dim=5000, output_dim=128, input_length=200))
-model.add(LSTM(64, return_sequences=True))
-model.add(LSTM(32))
-model.add(Dense(1, activation='sigmoid'))  # 'sigmoid' for binary classification
+# Define the RNN model
+def create_rnn_model():
+    model = Sequential()
+    model.add(Embedding(5000, 64, input_length=200))
+    model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(1, activation='sigmoid'))
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-# Step 3: Training the Model
-# Split your data into training and testing sets
-model.fit(train_sequences, train_labels, epochs=10, validation_data=(val_sequences, val_labels))
+def train_rnn_model(model, train_data, dev_data):
+    train_sequences, train_labels = preprocess_data(train_data)
+    dev_sequences, dev_labels = preprocess_data(dev_data)
 
-# Step 4: Evaluation
-test_loss, test_accuracy = model.evaluate(test_sequences, test_labels)
-print(f"Test Accuracy: {test_accuracy}")
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+    model.fit(train_sequences, train_labels, epochs=10, validation_data=(dev_sequences, dev_labels), callbacks=[early_stopping])
+    return model
+
+
+# Load data
+# train data is from hate_speech_train.npy
+train_data = np.load('data/hate_speech_train.npy', allow_pickle=True)
+# dev data is from hate_speech_dev.npy
+dev_data = np.load('data/hate_speech_dev.npy', allow_pickle=True)
+# test data is from hate_speech_test.npy
+test_data = np.load('data/hate_speech_test.npy', allow_pickle=True)
+
+
+rnn_model = create_rnn_model()
+rnn_model = train_rnn_model(rnn_model, train_data, dev_data)
+
+# Preprocess test data and evaluate the model
+test_sequences, test_labels = preprocess_data(test_data)
+predictions = rnn_model.predict(test_sequences)
+predictions = (predictions > 0.5).astype(np.int)
+
+# Confusion matrix and classification report
+print(confusion_matrix(test_labels, predictions))
+print(classification_report(test_labels, predictions))
