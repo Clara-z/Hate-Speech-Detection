620c2ce61aa3ae912d7ba9f94526c14e6fc47762
Qiang Zeng <qiangzen@usc.edu>
2023-12-12 00:16:15 -0800

    LSTM without hyperparam tuning.
    train acc ~ 70%, val acc ~ 50%. Add hyperparam tuning in next version.

--- LSTM.py
+++ LSTM.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
+import util
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
+from kerastuner.tuners import RandomSearch
 from sklearn.metrics import classification_report, confusion_matrix
 
 
@@ -17,7 +19,7 @@ def preprocess_data(data):
 
 
 # Define the RNN model
-def create_rnn_model():
+def create_lstm_model():
     model = Sequential()
     model.add(Embedding(5000, 64, input_length=200))
     model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
@@ -26,7 +28,7 @@ def create_rnn_model():
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-def train_rnn_model(model, train_data, dev_data):
+def train_lstm_model(model, train_data, dev_data):
     train_sequences, train_labels = preprocess_data(train_data)
     dev_sequences, dev_labels = preprocess_data(dev_data)
 
@@ -44,14 +46,15 @@ dev_data = np.load('data/hate_speech_dev.npy', allow_pickle=True)
 test_data = np.load('data/hate_speech_test.npy', allow_pickle=True)
 
 
-rnn_model = create_rnn_model()
-rnn_model = train_rnn_model(rnn_model, train_data, dev_data)
+rnn_model = create_lstm_model()
+rnn_model = train_lstm_model(rnn_model, train_data, dev_data)
 
 # Preprocess test data and evaluate the model
 test_sequences, test_labels = preprocess_data(test_data)
 predictions = rnn_model.predict(test_sequences)
-predictions = (predictions > 0.5).astype(np.int)
+predictions = (predictions > 0.5).astype(int)
 
-# Confusion matrix and classification report
-print(confusion_matrix(test_labels, predictions))
-print(classification_report(test_labels, predictions))
+# # Confusion matrix and classification report
+# print(confusion_matrix(test_labels, predictions))
+# print(classification_report(test_labels, predictions))
+util.eval(predictions, test_labels)
